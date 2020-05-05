a69f109257a3e1d2f57c6ec34f56fd6d79fcd922
McCoy Zhu <mz1945@nyu.edu>
2020-05-05 11:38:09 -0700

    webgit status

---
+++ web/projects.js
@@ -0,0 +1,14 @@
+const webProjects = [
+  {
+    title: "new Student()",
+    year: "2020",
+    identifier: "new-student-2020",
+    tags: ["HTML", "CSS", "Web Font", "Character Entities"],
+    bgImg: "new-student-2020/image.png",
+    detailViewType: "iframe",
+    iframeUrl: "https://zhumingcheng697.github.io/web-nyu-idm/css-diner/",
+    descriptions: [
+      'I created this webpage using pure CSS and HTML that indents and highlights the syntax of a Swift snippet.'
+    ]
+  }
+];
