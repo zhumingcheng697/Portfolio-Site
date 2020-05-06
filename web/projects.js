5a014d32b56909f33a7df81114c1eda326b70418
McCoy Zhu <mz1945@nyu.edu>
2020-05-06 09:39:45 -0700

    web projects

--- web/projects.js
+++ web/projects.js
@@ -10,5 +10,44 @@ const webProjects = [
     descriptions: [
       'I created this webpage using pure CSS and HTML that indents and highlights the syntax of a Swift snippet.'
     ]
+  },
+  {
+    title: "Toolbox",
+    year: "2019",
+    identifier: "toolbox-2019",
+    tags: ["p5.js", "CSS", "DOM", "API", "JSON", "Regex", "Geolocation"],
+    priority: 3,
+    bgImg: "toolbox-2019/image.png",
+    detailViewType: "iframe",
+    iframeUrl: "https://www.openprocessing.org/sketch/794151",
+    descriptions: [
+      'This is the final project for my <i>Creative Coding</i> class last year and I built it using JavaScript and p5.js only. I localized it for three languages, optimzied it for light and dark modes, and made it accessible to keyboard users.'
+    ]
+  },
+  {
+    title: "My MetroCard",
+    year: "2020",
+    identifier: "my-metro-card-2020",
+    tags: ["HTML", "CSS", "JavaScript", "Cookies", "Gesture Recognition"],
+    priority: 5,
+    bgImg: "my-metro-card-2020/image.png",
+    detailViewType: "iframe",
+    iframeUrl: "https://zhumingcheng697.github.io/web-nyu-idm/object-meditation/",
+    descriptions: [
+      'This website is the midterm project for my <i>Intro to Web Dev</i> class, and there is a really addictive game hidden inside. Be sure to swipe the MetroCard!'
+    ]
+  },
+  {
+    title: "OrganizeMyTabs",
+    year: "2020",
+    identifier: "organize-my-tabs-2020",
+    tags: ["Chrome", "JavaScript", "Extensions", "Messaging", "Storage"],
+    priority: 2,
+    bgImg: "organize-my-tabs-2020/image.png",
+    detailViewType: "video",
+    videoUrl: "organize-my-tabs-2020/video.mp4",
+    descriptions: [
+      'This is a <a target="_blank" href="https://github.com/zhumingcheng697/OrganizeMyTabs/releases">Chrome extension</a> I built that has a wide set of robust features for organizing tabs and windows.'
+    ]
   }
 ];
