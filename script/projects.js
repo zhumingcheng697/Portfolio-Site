0e045c3f46bcd8ef51b417cbce2214296e33c529
McCoy Zhu <mz1945@nyu.edu>
2020-05-05 20:22:22 -0700

    script projects

--- script/projects.js
+++ script/projects.js
@@ -1,3 +1,75 @@
 const scriptProjects = [
-  
+  {
+    title: "Morse Code Converter",
+    year: "2018",
+    identifier: "morse-code-converter-2018",
+    tags: ["Swift Playground", "Morse Code"],
+    bgImg: "morse-code-converter-2018/image.png",
+    detailViewType: "video",
+    videoUrl: "morse-code-converter-2018/video.mp4",
+    descriptions: [
+      'This is a script that converts between latin characters and morse code.'
+    ]
+  },
+  {
+    title: "QR Code Generator",
+    year: "2019",
+    identifier: "qr-code-generator-2019",
+    tags: ["Swift Playground", "QR Code"],
+    bgImg: "qr-code-generator-2019/image.png",
+    detailViewType: "video",
+    videoUrl: "qr-code-generator-2019/video.mp4",
+    descriptions: [
+      'This is a script that generates QR codes with texts, emails, SMSs, calender events, Wi-Fi passwords, address book contacts and more, with the option to customize the code size, quality, and color.'
+    ]
+  },
+  {
+    title: "ID Number Checker",
+    year: "2019",
+    identifier: "id-number-checker-2019",
+    tags: ["Swift Playground", "Regex"],
+    bgImg: "id-number-checker-2019/image.png",
+    detailViewType: "video",
+    videoUrl: "id-number-checker-2019/video.mp4",
+    descriptions: [
+      'This is a script that tracks if a string is valid for Chinese ID card numbers.'
+    ]
+  },
+  {
+    title: "Error Code Fixer",
+    year: "2018",
+    identifier: "error-code-fixer-2018",
+    tags: ["Swift Playground", "Character Encoding"],
+    bgImg: "error-code-fixer-2018/image.png",
+    detailViewType: "video",
+    videoUrl: "error-code-fixer-2018/video.mp4",
+    descriptions: [
+      'This is a script that decodes <a target="_blank" href="https://en.wikipedia.org/wiki/Percent-encoding">URL</a> error codes and <a target="_blank" href="https://en.wikipedia.org/wiki/GBK_(character_encoding)">GBK</a> error codes.'
+    ]
+  },
+  {
+    title: "EXIF Viewer",
+    year: "2019",
+    identifier: "exif-viewer-2019",
+    tags: ["Siri Shortcuts", "EXIF"],
+    priority: 2,
+    bgImg: "exif-viewer-2019/image.png",
+    detailViewType: "video",
+    videoUrl: "exif-viewer-2019/video.mp4",
+    descriptions: [
+      'This is a <a target="_blank" href="https://www.icloud.com/shortcuts/bbb932c436f547b6a8eccb99212c9e4b">pair</a> of <a target="_blank" href="https://www.icloud.com/shortcuts/048978d81b0f401f867810e2554d0f9a">Siri Shortcuts</a> that let you view the metadata of the photos and videos on your device.'
+    ]
+  },
+  {
+    title: "Date & Time Calculator",
+    year: "2019",
+    identifier: "date-n-time-calculator-2019",
+    tags: ["Siri Shortcuts", "Date"],
+    bgImg: "date-n-time-calculator-2019/image.png",
+    detailViewType: "video",
+    videoUrl: "date-n-time-calculator-2019/video.mp4",
+    descriptions: [
+      'This is a single <a target="_blank" href="https://www.icloud.com/shortcuts/4ba8c8ef9a484cc180f7eeaa4d3c831d">Siri Shortcut</a> that has a wide range of features related to calculating date and time.'
+    ]
+  }
 ];
