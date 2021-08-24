1a88e122c9300e9d2522ee7c5678f9c53aef7bf2
McCoy Zhu <mz1945@nyu.edu>
2021-08-23 22:17:28 -0700

    ADD: Dynamic Web Projects

--- web/projects.js
+++ web/projects.js
@@ -21,7 +21,7 @@ const webProjects = [
     detailViewType: "iframe",
     iframeUrl: "https://www.openprocessing.org/sketch/794151",
     descriptions: [
-      'This is the final project for my <i>Creative Coding</i> class last year and I built it using JavaScript and p5.js only. I localized it for three languages, optimzied it for light and dark modes, and made it accessible to keyboard users.'
+      'This is the final project for my <i>Creative Coding</i> class and I built it using JavaScript and p5.js only. I localized it for three languages, optimzied it for light and dark modes, and made it accessible to keyboard users.'
     ]
   },
   {
@@ -29,7 +29,7 @@ const webProjects = [
     year: "2020",
     identifier: "my-metro-card-2020",
     tags: ["HTML", "CSS", "JavaScript", "Cookies", "Gesture Recognition"],
-    priority: 5,
+    priority: 4,
     bgImg: "my-metro-card-2020/image.png",
     detailViewType: "iframe",
     iframeUrl: "https://zhumingcheng697.github.io/web-nyu-idm/object-meditation/",
@@ -48,6 +48,34 @@ const webProjects = [
     videoUrl: "organize-my-tabs-2020/video.mp4",
     descriptions: [
       'This is a <a target="_blank" href="https://github.com/zhumingcheng697/OrganizeMyTabs/releases">Chrome extension</a> I built that has a wide set of robust features for organizing tabs and windows.'
+    ]
+  },
+  {
+    title: "COVID | Headline",
+    year: "2020",
+    identifier: "covid-headline-2020",
+    tags: ["HTML", "CSS", "JavaScript", "React.js", "REST API"],
+    priority: 4.5,
+    bgImg: "covid-headline-2020/image.png",
+    detailViewType: "iframe",
+    iframeUrl: "https://zhumingcheng697.github.io/dynamic-web-midterm/",
+    descriptions: [
+      'This website is the midterm project for my <i>Dynamic Web Applications</i> class, and it displays the current COVID-19 statistics along side the top headlines in a given country or region.',
+      '<i>Unfortunately, the headline API currently does not work on publicly deployed sites due to plan restrictions. Feel free to clone my <a target="_blank" href="https://github.com/zhumingcheng697/dynamic-web-midterm">GitHub repo</a> and run it locally on your device to try out the full experience!</i>'
+    ]
+  },
+  {
+    title: "Rate My Classes",
+    year: "2020",
+    identifier: "rate-my-classes-2020",
+    tags: ["HTML", "CSS", "JavaScript", "Node.js", "React.js", "Express.js", "REST API", "Web Scraping", "Firebase"],
+    priority: 5,
+    bgImg: "rate-my-classes-2020/image.png",
+    detailViewType: "iframe",
+    iframeUrl: "https://unruffled-bose-857315.netlify.app/",
+    descriptions: [
+      'This website is the <a target="_blank" href="https://github.com/zhumingcheng697/dynamic-web-final">final project</a> for my <i>Dynamic Web Applications</i> class, and it allows students to review class information and ratings.',
+      '<i>Unfortunately, the <a target="_blank" href="https://stark-basin-35300.herokuapp.com/">backend</a> may take quite a long time to start up due to free-tier hosting.</i>'
     ]
   }
 ];
